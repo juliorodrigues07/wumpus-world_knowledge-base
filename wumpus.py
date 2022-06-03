048dcd8fc0dad13f67a38651153e40cd7fec9e92
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-06-03 15:04:21 -0300

    Initial commit

---
+++ wumpus.py
@@ -0,0 +1,13 @@
+from Cryptodome.Random import random
+
+
+initial_state = ['Nada', 'Nada', 'Nada', 'Nada', 'Nada']
+perception = initial_state
+
+
+def main():
+    print("Mundo do Wumpus!")
+
+
+if __name__ == '__main__':
+    main()
