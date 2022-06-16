7d42115b4ea1411dfb1c8fe45125e8c1ca3a2c01
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-06-16 00:39:26 -0300

    create: Inicializada a estrutura da base de conhecimento

---
+++ knowledge_base.py
@@ -0,0 +1,47 @@
+from environment import WumpusWorld
+from environment import random
+from environment import size
+from environment import player_position
+from environment import adj1
+from environment import adj2
+
+
+class KnowledgeBase:
+
+    def __init__(self, world : WumpusWorld):
+
+        self.visited = [[size - 2, 1]]
+        self.safe = [player_position, adj1, adj2]
+        self.possible_danger = None
+        self.danger = None
+        self.unknown = None
+        self.get_unknown_positions()
+
+    def get_unknown_positions(self):
+
+        positions = list()
+
+        for x in range(size):
+            for y in range(size):
+                if [x, y] != player_position and [x, y] != adj1 and [x, y] != adj2:
+                    positions.append([x, y])
+
+        self.unknown = positions
+
+
+# Teste
+def main():
+
+    world = WumpusWorld()
+    base = KnowledgeBase(world)
+
+    for i in range(size):
+        print(world.field[i])
+
+    print(base.unknown)
+    print(base.safe)
+    print(base.visited)
+
+
+if __name__ == '__main__':
+    main()
