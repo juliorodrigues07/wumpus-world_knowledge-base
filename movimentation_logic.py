fa800ed9b1a493f554c54241ba8f54d6e7792240
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-06-21 22:27:43 -0300

    feat: Adicionado "rascunho" da movimentação inicial do agente

--- movimentation_logic.py
+++ movimentation_logic.py
@@ -1,3 +1,4 @@
+from knowledge_base import KnowledgeBase
 from environment import WumpusWorld
 from environment import random
 
@@ -10,9 +11,10 @@ arrow_use = -10
 
 class Exploration:
 
-    def __init__(self, world : WumpusWorld):
+    def __init__(self, world : WumpusWorld, base : KnowledgeBase):
 
         self.world = world
+        self.base = base
         self.position = world.agent
         self.pointing = 'Direita'
         self.time = 0
@@ -20,6 +22,7 @@ class Exploration:
         self.gold = False
         self.alive = True
 
+        self.previous_position = world.agent
         self.wumpus_killed = 0
         self.pits_fallen = 0
         self.visited_positions = 0
@@ -33,4 +36,28 @@ class Exploration:
         return [[position[0] - 1, position[1]], [position[0], position[1] - 1],
                 [position[0] + 1, position[1]], [position[0], position[1] + 1]]
 
+    def moving(self):
+
+        n = self.possible_actions(self.position)
+        x = random.randrange(4)
+
+        next_action = n[x]
+        self.position = next_action
+
+        perception = self.world.get_perception(self.position)
+        self.base.tell_perception(self.position, perception)
+
     # def analyze perception(): tell
+
+
+# Teste
+def main():
+
+    world = WumpusWorld()
+    base = KnowledgeBase(world)
+    t = Exploration(world, base)
+    t.moving()
+
+
+if __name__ == '__main__':
+    main()
