e191eacd945f4d07e8f7ada75a4e040daad9ce95
Julio Rodrigues <julio.csr.271@aluno.ufsj.edu.br>
2022-06-28 00:36:18 -0300

    feat: Adicionado método para tratar somente posições válidas no mundo
    Os métodos invert_position (rotate_180) e calculate_actions foram movidos para o script movimentation_logic.py

--- src/utilities.py
+++ src/utilities.py
@@ -3,8 +3,14 @@ from Cryptodome.Random import random
 
 def possible_actions(position):
 
-    return [[position[0] - 1, position[1]], [position[0], position[1] - 1],
-            [position[0] + 1, position[1]], [position[0], position[1] + 1]]
+    x, y = position[0], position[1]
+    return [[x - 1, y], [x + 1, y], [x, y - 1], [x, y + 1]]
+
+
+def exclude_walls(position, size):
+
+    x, y = position[0], position[1]
+    return x > 0 and y > 0 and x < size - 1 and y < size - 1
 
 
 def random_pair(size):
@@ -13,57 +19,3 @@ def random_pair(size):
     y = random.randrange(size)
 
     return x, y
-
-
-def invert_position(previous_pointing):
-
-    if previous_pointing == 'Direita':
-        return 'Esquerda'
-    elif previous_pointing == 'Esquerda':
-        return 'Direita'
-    elif previous_pointing == 'Cima':
-        return 'Baixo'
-    else:
-        return 'Cima'
-
-
-def calculate_action(previous_position, previous_pointing, position):
-
-    pointing = str()
-    actions = 1
-    x1, y1 = previous_position[0], previous_position[1]
-    x2, y2 = position[0], position[1]
-
-    if x2 > x1 and y2 == y1:
-        pointing = 'Baixo'
-
-        if previous_pointing == 'Cima':
-            actions += 2
-        elif previous_pointing == 'Direita' or previous_position == 'Esquerda':
-            actions += 1
-
-    elif x2 < x1 and y2 == y1:
-        pointing = 'Cima'
-
-        if previous_pointing == 'Baixo':
-            actions += 2
-        elif previous_pointing == 'Direita' or previous_position == 'Esquerda':
-            actions += 1
-
-    elif x2 == x1 and y2 > y1:
-        pointing = 'Direita'
-
-        if previous_pointing == 'Esquerda':
-            actions += 2
-        elif previous_pointing == 'Cima' or previous_position == 'Baixo':
-            actions += 1
-
-    elif x2 == x1 and y2 < y1:
-        pointing = 'Esquerda'
-
-        if previous_pointing == 'Direita':
-            actions += 2
-        elif previous_pointing == 'Cima' or previous_position == 'Baixo':
-            actions += 1
-
-    return actions, pointing
